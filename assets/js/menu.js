46aa920c2d5b15318fdde9c9fbe876c8d9b37bda
taxa9060 <tahasr6953@gmail.com>
2025-05-08 14:41:44 +0330

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,7 @@
 const menuItems3 = document.querySelectorAll('.nav-item3');
 const menus3     = document.querySelectorAll('.mega-menu3');
 
+// باز و بسته کردن مگا منو با کلیک
 menuItems3.forEach(item => {
   item.addEventListener('click', e => {
     e.stopPropagation();
@@ -11,8 +12,15 @@ menuItems3.forEach(item => {
     });
   });
 });
+
+// بستن منو با کلیک خارج از منو یا آیتم‌ها
 window.addEventListener('click', e => {
   if (!e.target.closest('.nav-item3') && !e.target.closest('.mega-menu3')) {
     menus3.forEach(menu => menu.classList.remove('active'));
   }
+});
+
+// بستن منو هنگام اسکرول صفحه
+window.addEventListener('scroll', () => {
+  menus3.forEach(menu => menu.classList.remove('active'));
 });
